7202619384a42982637b1fef590da813a9098d58
huazhou <huazhou@ucfgroup.com>
2016-04-13 19:04:32 +0800

    idt 的 clock

---
+++ src/IDT/clock.asm
@@ -0,0 +1,329 @@
+; ==========================================
+; pm.asm
+; 编译方法：nasm pm.asm -o pm.bin
+; ==========================================
+
+%include	"pm.inc"	; 常量, 宏, 以及一些说明
+
+org	0100h
+	jmp	LABEL_BEGIN
+
+[SECTION .gdt]
+; GDT
+;                                                                         段基址,        段界限     ,                              属性
+LABEL_GDT:	                      Descriptor          0,                0,                                         0                  ; 空描述符
+LABEL_DESC_CODE32: Descriptor           0,             SegCode32Len - 1,    DA_C + DA_32   ; 非一致代码段
+LABEL_DESC_VIDEO:    Descriptor    0B8000h,     0ffffh,                              DA_DRW	       ; 显存首地址
+LABEL_DESC_DATA:   Descriptor    0,      DataLen-1, DA_DRW    ; Data
+LABEL_DESC_TEST:   Descriptor 0500000h,     0ffffh, DA_DRW
+LABEL_DESC_STACK:  Descriptor    0,     TopOfStack, DA_DRWA+DA_32; Stack, 32 位
+
+; GDT 结束
+
+GdtLen		equ	$ - LABEL_GDT	; GDT长度
+GdtPtr		dw	GdtLen - 1	; GDT界限
+					dd	0		; GDT基地址
+
+; GDT 选择子
+SelectorCode32		equ	LABEL_DESC_CODE32	- LABEL_GDT
+SelectorVideo		equ	LABEL_DESC_VIDEO	- LABEL_GDT
+SelectorData		equ	LABEL_DESC_DATA		- LABEL_GDT
+SelectorTest		equ	LABEL_DESC_TEST		- LABEL_GDT
+SelectorStack		equ	LABEL_DESC_STACK	- LABEL_GDT
+; END of [SECTION .gdt]
+
+[SECTION .data1]	 ; 数据段
+ALIGN	32
+[BITS	32]
+LABEL_DATA:
+BootMessage:		db	"Joey, I'm in protected mode!"
+OffsetPMMessage		equ	BootMessage - $$		;表示字符串BootMessage相对于本节的开始处（LABEL_DATA）的偏移
+StrTest:		db	"ABCDEFGHIJKLMNOPQRSTUVWXYZ", 0
+OffsetStrTest		equ	StrTest - $$
+DataLen			equ	$ - LABEL_DATA
+; END of [SECTION .data1]
+
+; IDT
+[SECTION .idt]
+ALIGN	32
+[BITS	32]
+LABEL_IDT:
+; 门                        目标选择子,            偏移, DCount, 属性
+%rep 32
+		Gate	SelectorCode32, SpuriousHandler,      0, DA_386IGate
+%endrep
+.020h:		Gate	SelectorCode32,    ClockHandler,      0, DA_386IGate
+%rep 95
+		Gate	SelectorCode32, SpuriousHandler,      0, DA_386IGate
+%endrep
+.080h:		Gate	SelectorCode32,  UserIntHandler,      0, DA_386IGate
+
+IdtLen		equ	$ - LABEL_IDT
+IdtPtr		dw	IdtLen - 1	; 段界限
+		dd	0		; 基地址
+; END of [SECTION .idt]
+
+; 全局堆栈段
+[SECTION .gs]
+ALIGN	32
+[BITS	32]
+LABEL_STACK:
+	times 25 db 0
+
+TopOfStack	equ	$ - LABEL_STACK - 1
+
+; END of [SECTION .gs]
+
+[SECTION .s16]
+[BITS	16]
+LABEL_BEGIN:
+	mov	ax, cs
+	mov	ds, ax
+	mov	es, ax
+	mov	ss, ax
+	mov	sp, 0100h
+
+	; 初始化 32 位代码段描述符
+	xor	eax, eax
+	mov	ax, cs
+	shl	eax, 4
+	add	eax, LABEL_SEG_CODE32
+	mov	word [LABEL_DESC_CODE32 + 2], ax
+	shr	eax, 16
+	mov	byte [LABEL_DESC_CODE32 + 4], al
+	mov	byte [LABEL_DESC_CODE32 + 7], ah
+
+	; 初始化数据段描述符
+	xor	eax, eax
+	mov	ax, ds
+	shl	eax, 4
+	add	eax, LABEL_DATA
+	mov	word [LABEL_DESC_DATA + 2], ax
+	shr	eax, 16
+	mov	byte [LABEL_DESC_DATA + 4], al
+	mov	byte [LABEL_DESC_DATA + 7], ah
+
+	; 初始化堆栈段描述符
+	xor	eax, eax
+	mov	ax, ds
+	shl	eax, 4
+	add	eax, LABEL_STACK
+	mov	word [LABEL_DESC_STACK + 2], ax
+	shr	eax, 16
+	mov	byte [LABEL_DESC_STACK + 4], al
+	mov	byte [LABEL_DESC_STACK + 7], ah
+
+	; 为加载 GDTR 作准备
+	xor	eax, eax
+	mov	ax, ds
+	shl	eax, 4
+	add	eax, LABEL_GDT		; eax <- gdt 基地址
+	mov	dword [GdtPtr + 2], eax	; [GdtPtr + 2] <- gdt 基地址
+
+	; 为加载 IDTR 作准备
+	xor	eax, eax
+	mov	ax, ds
+	shl	eax, 4
+	add	eax, LABEL_IDT		; eax <- idt 基地址
+	mov	dword [IdtPtr + 2], eax	; [IdtPtr + 2] <- idt 基地址
+
+	; 加载 GDTR
+	lgdt	[GdtPtr]
+
+	; 关中断
+	cli
+
+	; 加载 IDTR
+	lidt	[IdtPtr]
+
+	; 打开地址线A20
+	in	al, 92h
+	or	al, 00000010b
+	out	92h, al
+
+	; 准备切换到保护模式
+	mov	eax, cr0
+	or	eax, 1
+	mov	cr0, eax
+
+	; 真正进入保护模式
+	jmp	dword SelectorCode32:0	; 执行这一句会把 SelectorCode32 装入 cs,
+					; 并跳转到 SelectorCode32:0  处
+; END of [SECTION .s16]
+
+
+[SECTION .s32]; 32 位代码段. 由实模式跳入.
+[BITS	32]
+
+LABEL_SEG_CODE32:
+	mov	ax, SelectorData
+	mov	ds, ax			; 数据段选择子
+	mov	ax, SelectorTest
+	mov	es, ax			; 测试段选择子
+	mov	ax, SelectorVideo
+	mov	gs, ax			; 视频段选择子(目的)
+
+	mov	ax, SelectorStack
+	mov	ss, ax			; 堆栈段选择子
+
+	mov	esp, TopOfStack
+
+	call	Init8259A
+	int	080h
+	sti
+	jmp	$
+
+; 下面显示一个字符串
+	mov ecx, 28
+	xor	esi, esi
+	xor	edi, edi
+	mov	edi, 0
+	mov	esi, OffsetPMMessage	; 源数据偏移
+.show:
+	mov	ah, 0Ch			; 0000: 黑底    1100: 红字
+	mov	al, [ds:esi]
+	mov	[gs:edi], ax
+	inc edi
+	inc edi
+	inc esi
+	loop .show
+; 显示完毕
+
+	call	TestRead
+	call	TestWrite
+	call	TestRead
+
+	; 到此停止
+	jmp	$
+
+; Init8259A ---------------------------------------------------------------------------------------------
+Init8259A:
+	mov	al, 011h
+	out	020h, al	; 主8259, ICW1.
+	call	io_delay
+
+	out	0A0h, al	; 从8259, ICW1.
+	call	io_delay
+
+	mov	al, 020h	; IRQ0 对应中断向量 0x20
+	out	021h, al	; 主8259, ICW2.
+	call	io_delay
+
+	mov	al, 028h	; IRQ8 对应中断向量 0x28
+	out	0A1h, al	; 从8259, ICW2.
+	call	io_delay
+
+	mov	al, 004h	; IR2 对应从8259
+	out	021h, al	; 主8259, ICW3.
+	call	io_delay
+
+	mov	al, 002h	; 对应主8259的 IR2
+	out	0A1h, al	; 从8259, ICW3.
+	call	io_delay
+
+	mov	al, 001h
+	out	021h, al	; 主8259, ICW4.
+	call	io_delay
+
+	out	0A1h, al	; 从8259, ICW4.
+	call	io_delay
+
+	mov	al, 11111110b	; 仅仅开启定时器中断
+	;mov	al, 11111111b	; 屏蔽主8259所有中断
+	out	021h, al	; 主8259, OCW1.
+	call	io_delay
+
+	mov	al, 11111111b	; 屏蔽从8259所有中断
+	out	0A1h, al	; 从8259, OCW1.
+	call	io_delay
+
+	ret
+; Init8259A ---------------------------------------------------------------------------------------------
+
+io_delay:
+	nop
+	nop
+	nop
+	nop
+	ret
+
+_ClockHandler:
+ClockHandler	equ	_ClockHandler - $$
+	inc	byte [gs:((80 * 0 + 70) * 2)]	; 屏幕第 0 行, 第 70 列。
+	mov	al, 20h
+	out	20h, al				; 发送 EOI
+	iretd
+
+_UserIntHandler:
+UserIntHandler	equ	_UserIntHandler - $$
+	mov	ah, 0Ch				; 0000: 黑底    1100: 红字
+	mov	al, 'I'
+	mov	[gs:((80 * 0 + 70) * 2)], ax	; 屏幕第 0 行, 第 70 列。
+	iretd
+
+_SpuriousHandler:
+SpuriousHandler	equ	_SpuriousHandler - $$
+	mov	ah, 0Ch				; 0000: 黑底    1100: 红字
+	mov	al, '!'
+	mov	[gs:((80 * 0 + 75) * 2)], ax	; 屏幕第 0 行, 第 75 列。
+	jmp	$
+	iretd
+
+; ------------------------------------------------------------------------
+TestRead:
+	xor	esi, esi
+	mov	ecx, 8
+.loop:
+	mov	al, [es:esi]
+	call	DispAL
+	inc	esi
+	loop	.loop
+
+	ret
+; TestRead 结束-----------------------------------------------------------
+
+
+; ------------------------------------------------------------------------
+TestWrite:
+	push	esi
+	push	edi
+	xor	esi, esi
+	xor	edi, edi
+	mov	esi, OffsetStrTest	; 源数据偏移
+	cld
+.1:
+	lodsb
+	test	al, al
+	jz	.2
+	mov	[es:edi], al
+	inc	edi
+	jmp	.1
+.2:
+
+	pop	edi
+	pop	esi
+
+	ret
+; TestWrite 结束----------------------------------------------------------
+
+; ------------------------------------------------------------------------
+;
+; ------------------------------------------------------------------------
+DispAL:
+	push	ecx
+	push	edx
+
+	mov	ah, 0Ch			; 0000: 黑底    1100: 红字
+
+	mov	[gs:edi], ax
+	add	edi, 2
+
+	pop	edx
+	pop	ecx
+
+	ret
+; DispAL 结束-------------------------------------------------------------
+
+SegCode32Len	equ	$ - LABEL_SEG_CODE32
+; END of [SECTION .s32]
+
